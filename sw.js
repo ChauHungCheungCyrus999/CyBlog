979ec29010e9782f60a29e3fe0ca2f16121bc72f
Chau Hung Cheung <chauhungcheung@ChaudeMacBook-Air-2.local>
2026-07-05 12:48:42 +0800

    Site updated: 2026-07-05 12:48:41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1783226855827";const preCache = ["/CyBlog/images/taichi.png","/CyBlog/images/banner1.webp","/CyBlog/css/loader.css","/CyBlog/css/style.css","/CyBlog/js/script.js"];const cacheDomain = [
+const VERSION = "1783226919921";const preCache = ["/CyBlog/images/taichi.png","/CyBlog/images/banner1.webp","/CyBlog/css/loader.css","/CyBlog/css/style.css","/CyBlog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
